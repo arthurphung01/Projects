0e22090862344757c51b4950875dc8d96084052b
arthurphung01 <89538580+arthurphung01@users.noreply.github.com>
2023-08-22 18:46:00 -0700

    Add files via upload

---
+++ Pomodoro/app.js
@@ -0,0 +1,49 @@
+// global variables
+const time_el = document.querySelector('.container .time');
+const start_btn = document.getElementById('start');
+const pause_btn = document.getElementById('pause');
+const reset_btn = document.getElementById('reset');
+
+let targetTime = 1 * 60 * 1000;
+let currentTime = 0;
+let interval = null;
+
+// event listeners
+start_btn.addEventListener('click', start);
+pause_btn.addEventListener('click', stop);
+reset_btn.addEventListener('click', reset);
+
+
+// update timer
+function timer () {
+
+    // format out time
+    let mins = Math.floor(targetTime / (1000 * 60));
+    let secs = Math.abs(targetTime % (1000 * 60));
+
+    time_el.innerText = `${mins/1000 < 10 ? '0' : ''}${mins/1000}:${secs/1000 < 10 ? '0' : ''}${secs/1000}`;
+}
+
+function start () {
+    if (targetTime > 0) {
+        clearInterval(interval);
+        interval = setInterval(() => {
+            targetTime -= 1000;
+            timer();
+            if (targetTime  <= 0)  {
+                clearInterval(interval);
+                time_el.textContent = 'Time\'s up!';
+            }
+        }, 1000)
+    }
+}
+
+function stop ()  {
+    clearInterval(interval);
+}
+
+function reset () {
+    stop();
+    clearInterval(interval);
+    time_el.innerHTML =  '25:00';
+}
